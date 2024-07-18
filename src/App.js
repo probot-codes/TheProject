2a1a3ffd0eb1a52d1cc6bae9b51c08bdec31c1a5
Prabhat Sharma <ps25082005@gmail.com>
2024-07-18 08:57:12 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-// src/App.js
 import React, { useState } from 'react';
 import './App.css';
+import subwaySurferVideo from './vid.mp4'; // Ensure this file is in the src folder
 
 function App() {
     const [youtubeLink, setYoutubeLink] = useState('');
@@ -19,14 +19,16 @@ function App() {
     return (
         <div className="container">
             <div className="gameplay">
-                <iframe 
+                <video 
                     width="100%" 
                     height="100%" 
-                    src="https://www.youtube.com/embed/your_subway_surfer_video_id_here?autoplay=1&mute=1" 
-                    frameBorder="0" 
-                    allow="autoplay; encrypted-media" 
-                    allowFullScreen
-                ></iframe>
+                    autoPlay 
+                    muted
+                    loop
+                >
+                    <source src={subwaySurferVideo} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
             </div>
             <div className="youtube-video">
                 {videoId && (
